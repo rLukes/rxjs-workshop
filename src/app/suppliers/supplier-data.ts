b2f7bbdfa3fa311935c21c6a6ee7ae9fb0201e8c
Ruben Lukes <lukesruben@gmail.com>
2019-10-26 21:28:34 +0200

    supplier data

--- src/app/suppliers/supplier-data.ts
+++ src/app/suppliers/supplier-data.ts
@@ -1,68 +1,67 @@
-import { Supplier } from './supplier';
+import { Supplier } from "./supplier";
 
 export class SupplierData {
+  static suppliers: Supplier[] = [
+    {
+      id: 1,
+      name: "ARS Supply",
+      cost: 16.95,
+      minQuantity: 12
+    },
+    {
+      id: 2,
+      name: "Standard-FRAME",
+      cost: 15.95,
+      minQuantity: 24
+    },
 
-    static suppliers: Supplier[] = [
-        {
-            id: 1,
-            name: 'Acme Gardening Supply',
-            cost: 16.95,
-            minQuantity: 12
-        },
-        {
-            id: 2,
-            name: 'Standard Gardening',
-            cost: 15.95,
-            minQuantity: 24
-        },
-
-        {
-            id: 3,
-            name: 'Acme Gardening Supply',
-            cost: 12,
-            minQuantity: 6
-        },
-        {
-            id: 4,
-            name: 'Acme General Supply',
-            cost: 25,
-            minQuantity: 2
-        },
-        {
-            id: 5,
-            name: 'Acme General Supply',
-            cost: 2,
-            minQuantity: 24
-        },
-        {
-            id: 6,
-            name: 'Acme Tool Supply',
-            cost: 4,
-            minQuantity: 12
-        },
-        {
-            id: 7,
-            name: 'Tools Are Us',
-            cost: 8,
-            minQuantity: 8
-        },
-        {
-            id: 8,
-            name: 'Acme Tool Supply',
-            cost: 4,
-            minQuantity: 12
-        },
-        {
-            id: 9,
-            name: 'Acme Game Supply',
-            cost: 20,
-            minQuantity: 6
-        },
-        {
-            id: 10,
-            name: 'Acme General Supply',
-            cost: 12,
-            minQuantity: 12
-        }
-    ];
+    {
+      id: 3,
+      name: "Hall of Frame",
+      cost: 12,
+      minQuantity: 6
+    },
+    {
+      id: 4,
+      name: "FAME FRAME",
+      cost: 25,
+      minQuantity: 2
+    },
+    {
+      id: 5,
+      name: "DBR-FRAME",
+      cost: 2,
+      minQuantity: 24
+    },
+    {
+      id: 6,
+      name: "SCR-FRAME",
+      cost: 4,
+      minQuantity: 12
+    },
+    {
+      id: 7,
+      name: "ITALIAN-CO",
+      cost: 8,
+      minQuantity: 8
+    },
+    {
+      id: 8,
+      name: "ROMAN FRAME ES",
+      cost: 4,
+      minQuantity: 12
+    },
+    {
+      id: 9,
+      name: "FRAME Supply",
+      cost: 20,
+      minQuantity: 6
+    },
+    {
+      id: 10,
+      name: "PHOTO FRAME Supply",
+      cost: 12,
+      minQuantity: 12
+    }
+  ];
 }
